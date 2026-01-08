5b9a4ab3ed4d6101e7373ffde6393c3708b3b05b
Akshansh <akshanshdubey.dev@gmail.com>
2026-01-09 03:04:38 +0530

    Completed bookings flow with seat management and role-based access

--- scripts/deleteAllTrips.js
+++
@@ -1,8 +0,0 @@
-//delete all trips
-const connect = require('./config/db');
-connect();
-
-const Trip = require('../models/Trip');
-Trip.deleteMany({}).then(() => console.log('Trips deleted'));
-
-deleteAllTrips();
